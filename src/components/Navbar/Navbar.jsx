b12269293afeb18f2d7af6f551f3cd23cd4f2132
Andrey <sereginaa20@yandex.ru>
2021-07-06 14:33:29 +0400

    first commit

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,43 @@
+import React from "react";
+import {
+  AppBar,
+  Toolbar,
+  IconButton,
+  Badge,
+  Typography,
+} from "@material-ui/core";
+import { ShoppingCart } from "@material-ui/icons";
+import { Link } from "react-router-dom";
+import logo from "../../assets/commerce.png";
+import useStyles from "./styles";
+
+const Navbar = ({totalItems}) => {
+  const classes = useStyles();
+  return (
+    <>
+      <AppBar position="fixed" className={classes.appbar} color="inherit">
+        <Toolbar>
+          <Typography component={Link} to="/" variant="h6" className={classes.title} color="inherit">
+            <img
+              src={logo}
+              alt="Commerce.js"
+              height="25px"
+              className={classes.image}
+            />
+            My-shop
+          </Typography>
+          <div className={classes.grow} />
+          <div className="classes.button">
+            <IconButton component={Link} to="/cart" aria-label="show cart item" color="inherit">
+              <Badge badgeContent={totalItems} color="secondary">
+                <ShoppingCart />
+              </Badge>
+            </IconButton>
+          </div>
+        </Toolbar>
+      </AppBar>
+    </>
+  );
+};
+
+export default Navbar;
